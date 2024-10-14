24464226ad1552ff7b4b506323c23471d7792366
Andrey Starostin <161159835+a-lessnick@users.noreply.github.com>
2024-10-14 11:55:01 +0300

    Before deploy.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,11 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '130.193.41.218', 'localhost', 'taski-yandex.zapto.org']
+ALLOWED_HOSTS = ['0.0.0.0',
+                 '127.0.0.1',
+                 '130.193.41.218',
+                 'localhost',
+                 'taski-yandex.zapto.org']
 
 
 # Application definition
